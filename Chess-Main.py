a8952083a83532f5abe737530186dfc4570a0ebf
William Moffett <TetonRetinalInstitute@MacBook-Pro-3.local>
2021-01-27 18:35:20 -0700

    first commit

---
+++ Chess-Main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 27 17:41:26 2021
+
+@author: Albert einstein jr aka William Moffett
+"""
+
+import chess
+def main():
+    board = chess.Board()
+    print(board)
+main()
+# https://medium.com/dscvitpune/lets-create-a-chess-ai-8542a12afef
+#yeah
